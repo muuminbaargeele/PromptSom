3bc14eb6d18f6e37dbb589f69832eb26ce6be4c7
muuminbaargeele <muuminbaargeele1@gmail.com>
2023-11-02 23:27:31 +0300

    sidbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Chat from './components/Chat'
 const App: React.FC = () => {
   postData()
   return (
-    <div className="flex flex-row bg-darkLightGreen h-screen w-full">
+    <div className="flex bg-darkLightGreen h-screen w-full">
       <Sidebar />
       <Chat />
     </div>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import { RiUserFill } from 'react-icons/ri'
-import { promptSomDevs } from '../lib/data'
+import React from "react";
+// import { RiUserFill } from 'react-icons/ri'
+import { FaUserTie } from "react-icons/fa";
+import { promptSomDevs } from "../lib/data";
 
 const Sidebar: React.FC = () => {
   return (
-    <section className="fixed left-0 hidden lg:flex flex-col justify-between  h-full bg-darkGreen text-white w-80 px-[1.875rem]">
+    <section className="hidden lg:flex flex-col justify-between  h-full bg-darkGreen text-grayColor w-80 px-[1.875rem]">
       <h1 className="font-slackSide text-[3rem] text-primaryColor font-bold">
         PromptSom
       </h1>
 
-      <div className="pb-10">
+      <div className="pb-10 ">
         <div className="border-b border-primaryColor pb-5">
           {promptSomDevs.map((dev) => (
-            <ul key={dev.id} className="flex items-center gap-[5px]">
-              <RiUserFill className=" h-8 w-8" />
-              <li className="text-[1rem] font-popins py-3">{dev.name}</li>
+            <ul key={dev.id} className="flex items-center gap-[0.3rem]">
+              <div className="flex gap-2 my-1 items-end">
+                <FaUserTie className=" h-6 w-6" />
+                <li className="text-[0.75rem] font-popins">{dev.name}</li>
+              </div>
             </ul>
           ))}
         </div>
 
-        <p className="opacity-50 pt-5 text-[1rem] font-popins font-bold leading-7">
+        <p className="opacity-50 pt-5 text-[0.75rem] font-popins font-medium leading-7">
           These five students worked together to create a chatbot that
           understands Somali. Their goal was to make a special version of AI
           that is easy for Somali speakers to use. They succeeded in making a
@@ -28,7 +31,7 @@ const Sidebar: React.FC = () => {
         </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
