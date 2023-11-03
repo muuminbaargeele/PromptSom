a69e21afdb77825511acfa42700cf9c1d5a93f56
muuminbaargeele <muuminbaargeele1@gmail.com>
2023-11-03 15:42:32 +0300

    Alignment checking

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -8,35 +8,7 @@ async function postData() {
     console.log('Data posted successfully:', response.data);
     return response.data;
   } catch (error) {
-    // if (axios.isAxiosError(error)) {
-    //   // If the error is an AxiosError, we know it will have certain properties
-    //   const axiosError = error as AxiosError;
-    //   if (axiosError.response) {
-    //     console.error('Error data:', axiosError.response.data);
-    //     console.error('Error status:', axiosError.response.status);
-    //     console.error('Error headers:', axiosError.response.headers);
-    //   } else if (axiosError.request) {
-    //     console.error('Error request:', axiosError.request);
-    //   } else {
-    //     console.error('Error message:', axiosError.message);
-    //   }
-    // } else if (error instanceof Error) {
-    //   // This is a generic error
-    //   console.error('Error message:', error.message);
-    // } else {
-    //   // The error is of unknown type
-    //   console.error('An unexpected error occurred');
-    // }
-    // return null;
     console.log(error)
   }
 }
-
-// Example of how to use the postData function
-// postData().then((data) => {
-//   if (data) {
-//     console.log('Received data:', data);
-//   }
-// });
-
 export default postData
--- src/components/Response.tsx
+++ src/components/Response.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { FaUser, FaRobot } from 'react-icons/fa'
-import { BiSolidUserVoice } from 'react-icons/bi'
+import React from "react";
+import { FaUser, FaRobot } from "react-icons/fa";
+import { BiSolidUserVoice } from "react-icons/bi";
 
 const Response: React.FC = () => {
   return (
-    <section className="flex flex-col gap-3 h-full overflow-y-scroll active-scroll absolute pr-5 bg-transparent">
+    <section className="flex flex-col gap-3 h-[435px] overflow-y-scroll active-scroll absolute pr-5 bg-transparent">
       <div className="flex flex-col gap-3">
         <div className="flex items-center gap-3">
-          <span className="border border-grayColor/50 rounded-2xl p-3">
+          <span className="border  border-grayColor/50 rounded-2xl p-[14px]">
             <FaUser className="text-white h-12 w-12" />
           </span>
-          <span className="flex-1 pl-4 py-7 text-white font-popins font-bold text-sm border border-grayColor rounded-2xl">
+          <span className="flex-1 pl-4 py-7 text-white font-popins font-bold text-sm border border-grayColor/50 rounded-2xl">
             hey my name is ahmed khaliif i am from somalia i love coding i am
             flutter developer
           </span>
         </div>
 
         <div className="flex items-start gap-3 ">
-          <span className="border border-grayColor/50 rounded-2xl p-3">
-            <FaRobot className="text-white h-10 w-10" />
+          <span className="border border-grayColor/50 rounded-2xl p-[14px]">
+            <FaRobot className="text-white h-12 w-12" />
           </span>
-          <div className="flex flex-col justify-between border border-grayColor/50 rounded-2xl px-4 py-7 h-[22rem]  overflow-y-scroll active-scroll">
+          <div className="flex flex-col justify-between border border-grayColor/50 rounded-2xl px-4 py-7  overflow-y-scroll active-scroll">
             <p className="text-white text-sm font-popins font-normal ">
               That's great, Ahmed! Flutter is a popular framework for developing
               cross-platform mobile applications. It's great to hear that you
@@ -29,14 +29,14 @@ const Response: React.FC = () => {
               skills. If you have any questions or need any assistance, feel
               free to ask!
             </p>
-            <span className="self-end mr-4 cursor-pointer">
+            <span className="self-end  cursor-pointer">
               <BiSolidUserVoice className="text-white h-10 w-10 " />
             </span>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Response
+export default Response;
